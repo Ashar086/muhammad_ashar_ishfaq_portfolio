a7efcc656bcc261811d4e0febf6c41514c1c625b
Muhammad Ashar Ishfaq <161827453+Ashar086@users.noreply.github.com>
2024-07-27 17:53:48 +0500

    Add files via upload

---
+++ assets/js/email.js
@@ -0,0 +1,22 @@
+function emailSend() {
+
+let name = document.getElementById('name').value;
+let email = document.getElementById('email').value;
+let subject = document.getElementById('subject').value;
+let msgBody = document.getElementById('message').value;
+
+Email.send({
+    Host : "sandbox.smtp.mailtrap.io",
+    port: 2525,
+    Username : "0788e03750ae70",
+    Password : "d8118c3b960d31",
+    To : 'pyles.kit@gmail.com',
+    From : "pyles.kit@gmail.com",
+    Subject : `Landing Page Contact, RE: ${subject}`,
+    Body : `Name: ${name}<br>
+    EMail Address: ${email}<br>
+    Message: ${msgBody}`
+}).then(
+  message => alert("Your message has been sent.")
+);
+}
